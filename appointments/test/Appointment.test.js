382df697133154e0efd06110f8970d3a2a8826bd
James <mr.woods1994@hotmail.co.uk>
2021-01-09 16:16:32 +0000

    added appointment.js to src file

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,3 +1,15 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Appointment } from '..src/Appointment'
+
 describe('Appointment', () => {
+	it('renders the customer first name', () => {
+		const customer = { firstName: 'Ashley'}
+		const component = <Appointment customer={customer} />
+		const container = document.createElement('div')
+		document.body.appendChild(container)
+		ReactDOM.render(component, container)
+		expect(document.body.testContent).toMatch('Ashley')
+	})
 
 });
