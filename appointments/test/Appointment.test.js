a84fe980ffb37957fb3b688c0b3c7a4ede91b7b3
James <mr.woods1994@hotmail.co.uk>
2021-01-10 20:44:15 +0000

    added new tests to check if lis are being created and added a function to create a string from the time

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -26,12 +26,34 @@ describe('Appointment', () => {
 })
 describe('AppointmentsDayView', () => {
 	let container
+	let appointments
+	let today
+
 	beforeEach(() => {
 		container = document.createElement('div')
+		today = new Date()
+		appointments = [
+		{ startsAt: today.setHours(12, 0) },
+		{ startsAt: today.setHours(13, 0) }
+		]
+
 	})
 	const render = (component) => ReactDOM.render(component, container)
+
 	it('renders a div with the right ID', () => {
-		render(<AppointmentsDayView Appointments={[]} />)
-		expect(container.querySelector('div#appointmentsdayView')).not.toBeNull()
+		render(<AppointmentsDayView appointments={appointments} />)
+		expect(container.querySelector('div#appointmentsDayView')).not.toBeNull()
+	})
+	it('renders multiple appointments in an ol element', () => {
+
+		render(<AppointmentsDayView appointments={appointments} />)
+		expect(container.querySelector('ol')).not.toBeNull()
+		expect(container.querySelector('ol').children).toHaveLength(2)
+	})
+	it('renders each appointment in an li', () => {
+		render(<AppointmentsDayView appointments={appointments} />)
+		expect(container.querySelectorAll('li')).toHaveLength(2)
+		expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00')
+		expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00')
 	})
 })
